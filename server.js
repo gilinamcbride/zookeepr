87a09860564f24988013ed72df63605a070984cd
Gilina McBride <gilinamcbride@gmail.com>
2022-06-19 16:03:15 -0400

    add post functions to add more animals

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const { animals } = require("./data/animals");
+const fs = require("fs");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 // instantiate the server
 // assign express to the app variable so that we can later chain on methods
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
   // Note that we save the animalsArray as filteredResults here:
@@ -54,12 +61,60 @@ function findById(id, animalsArray) {
   return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, "./data.animals.json"),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  // return finished code to post route for response
+  return animal;
+}
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== "string") {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== "string") {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== "string") {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
+// call back fx to get information using the URL
+app.get("/api/animals", (req, res) => {
+  let results = animals;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+
+// call back fx to give data to the server
+// package up the data as an obj and send to the server - req.body property is where we can access that data on the server side and do something with it
+app.post("/api/animals", (req, res) => {
+  //req.body is where our incoming content will be
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+  // add animal to json file and animals array in this function
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send("The animal is not properly formatted.");
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(req.body);
+  }
+});
+
+// call back fx to get information using the URL
 app.get("/api/animals/:id", (req, res) => {
-  //   let results = animals;
-  //   if (req.query) {
-  //     results = filterByQuery(req.query, results);
-  //   }
-  //   res.json(results);
   const result = findById(req.params.id, animals);
   if (result) {
     res.json(result);
